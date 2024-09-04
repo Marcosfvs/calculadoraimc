d9e17c063bcc1e44297cce8f7b395b9caed6727e
Marcos Silva <marcos.fvsilva11@hotmail.com>
2024-09-04 22:56:03 +0100

    resultado segunda tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             val pesoStr: String = edtPeso.text.toString()
             val alturaStr: String = edtAltura.text.toString()
             //.isNotEmpty é usado para checar se a string está vazia, pode usar: == "" como no JS
+
             if (pesoStr == "" || alturaStr == "") {
                 Snackbar
                     .make(
@@ -37,8 +39,12 @@ class MainActivity : AppCompatActivity() {
 
                 val altura2 = altura * altura
                 val imc = peso / altura2
-                println("Ação do botão " + imc)
+                //método para navegar para a próxima tela
+                val intent = Intent(this,ResultActivity::class.java)
+                intent.putExtra(KEY_RESUL_IMC,imc)
+                startActivity(intent)
             }
+
         }
     }
 }
