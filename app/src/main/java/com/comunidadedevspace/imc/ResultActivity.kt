74f24173f0ff1f6fcba11f1231eb8d0fb7a1e563
Marcos Silva <marcos.fvsilva11@hotmail.com>
2024-09-10 22:06:16 +0100

    Cores no resultado

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -26,20 +27,46 @@ class ResultActivity : AppCompatActivity() {
 
         tvResult.text = result_imc.toString()
 
-        var result_classificacao:String = ""
-        if (result_imc  <= 18.5f){
+        /*var result_classificacao: String = ""
+        if (result_imc <= 18.5f) {
             result_classificacao = "MAGREZA"
-        } else if (18.5f < result_imc  && result_imc  <= 24.9f) {
+        } else if (18.5f < result_imc && result_imc <= 24.9f) {
             result_classificacao = "NORMAL"
-        } else if (24.9f < result_imc  && result_imc  <= 29.9f){
-            result_classificacao ="OBESIDADE I"
-        } else if (29.9f < result_imc  && result_imc  < 39.9f){
+        } else if (24.9f < result_imc && result_imc <= 29.9f) {
+            result_classificacao = "OBESIDADE I"
+        } else if (29.9f < result_imc && result_imc < 39.9f) {
             result_classificacao = "OBESIDADE II"
         } else {
             result_classificacao = "OBESIDADE GRAVE"
+        }*/
+    var result_classificacao =""
+
+        when {
+            result_imc <= 18.5f -> {
+                result_classificacao = "MAGREZA"
+                tvClassificacao.setTextColor(ContextCompat.getColor(this, R.color.perigo))
+            }
+
+            18.5f < result_imc && result_imc <= 24.9f -> {
+                result_classificacao = "NORMAL"
+                tvClassificacao.setTextColor(ContextCompat.getColor(this, R.color.normal))
+            }
+
+            24.9f < result_imc && result_imc <= 29.9f -> {
+                result_classificacao = "OBESIDADE I"
+                tvClassificacao.setTextColor(ContextCompat.getColor(this, R.color.alerta))
+            }
+
+            29.9f < result_imc && result_imc < 39.9f -> {
+                result_classificacao = "OBESIDADE II"
+                tvClassificacao.setTextColor(ContextCompat.getColor(this, R.color.perigo))
+            }
+
+            else -> {
+                result_classificacao = "OBESIDADE GRAVE"
+                tvClassificacao.setTextColor(ContextCompat.getColor(this, R.color.perigo))
+            }
         }
-
         tvClassificacao.text = result_classificacao
-
     }
 }
